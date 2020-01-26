ef6ba34480f2f629e18f7c4762cf522e9b0eacf7
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-01-26 21:02:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+function Food({ name }) {
+  return <h1>I love {name}</h1>; 
+}
+
+constdILike = [
+  {name = "Kimchi"},
+  {name = "Samgyeopsal"},
+  {name = "Bibimbap"}
+];
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      {FoodILike.map(dish => <Food name={dish.name} />)}
     </div>
   );
 }
