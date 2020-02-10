4d5034f8cb9467fbd2dd31bfc254b3af1ad3d560
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-10 14:42:44 +0900

    styleIt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import Movie from 'movie';
+import Movie from './Movie';
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -17,7 +18,11 @@ class App extends React.Component{
   render(){
     const { isLoading, movies } = this.state;
     return (
-    <div>{isLoading ? "Loading" : movies.map(movie => (
+    <section class="container">{isLoading ? (<div class="loader">
+      <span class="loader__text">Loading...</span>
+    </div>) : (
+      <div class="mpvies">
+      {movies.map(movie => (
       <Movie
       key={movie.id}
       id={movie.id} 
@@ -25,7 +30,9 @@ class App extends React.Component{
       title={movie.title} 
       summary={movie.summary} 
       poster={movie.medium.cover.image} />
-    ))}</div>
+    ))}
+      </div>
+      )}</section>
    );
   }
 }
