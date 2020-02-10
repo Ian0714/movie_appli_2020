64019a430d5660e335c97b805d8117df325b3f43
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-10 10:14:11 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({ name, rating }) {
-  return (
-  <div>
-    <h1>I love {name}</h1>
-    <h4>{rating}/5.0</h4>
-    </div>
-    ); 
-}
-
-const FoodILike = [
-  {id: 1, name: "Kimchi", rating: 5},
-  {id: 2, name: "Samgyeopsal", rating: 4.9},
-  {id: 3, name: "Bibimbap", rating: 4.2}
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function App() {
-  return (
-    <div>
-      {FoodILike.map(dish => (<Food key={dish.id} name={dish.name} rating={dish.rating}/>))}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {};
+  minus = () => {};
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
