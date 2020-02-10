253cd5c651c9448b2f58fb124b4888b95983c3b3
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-10 13:43:15 +0900

    isLoading

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from 'react';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log('Hello');
-  }
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count:current.count+1 }))
-  };
-  minus = () => {
-    this.setState(current => ({ count:current.count-1 }))
+    isLoading: true,
+    movies: []
   };
   componentDidMount(){
-    console.log('Component rendered');
-  }
-  componentDidUpdate(){
-    console.log('Updated');
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000) 
   }
   render(){
+    const { isLoading } = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    <div>{isLoading ? "Loading" : "We Are Ready"}</div>
+   );
   }
 }
 
