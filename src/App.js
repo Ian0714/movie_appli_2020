5ae7b50447320d9c7af243fbcde7d899467aa3c9
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-11 15:52:14 +0900

    addGenre

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ class App extends React.Component{
   render(){
     const { isLoading, movies } = this.state;
     return (
-    <section class="container">{isLoading ? (<div class="loader">
-      <span class="loader__text">Loading...</span>
+    <section className="container">{isLoading ? (<div className="loader">
+      <span className="loader__text">Loading...</span>
     </div>) : (
-      <div class="mpvies">
+      <div className="mpvies">
       {movies.map(movie => (
       <Movie
       key={movie.id}
@@ -29,7 +29,8 @@ class App extends React.Component{
       year={movie.year} 
       title={movie.title} 
       summary={movie.summary} 
-      poster={movie.medium.cover.image} />
+      poster={movie.medium.cover.image}
+      genres={movie.genres} />
     ))}
       </div>
       )}</section>
--- src/movie.js
+++ src/movie.js
@@ -2,13 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
 return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
       <div class="movie__data">
         <h3 class="movie__title">{title}</h3>
         <h5 class="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, number) => (
+          <li key={number} className="genres__genre">{genre}</li>
+          ))}
+        </ul>
         <p class="movie__summary">{summary}</p>
       </div>
     </div>
@@ -20,7 +25,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     titie: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
