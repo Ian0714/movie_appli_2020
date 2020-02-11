40e83fafde737bde6e64c3422e857e4303edb072
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-11 16:28:25 +0900

    designButError

--- src/movie.js
+++ src/movie.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie({ year, title, summary, poster, genres }) {
 return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
       <div class="movie__data">
         <h3 class="movie__title">{title}</h3>
         <h5 class="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, number) => (
           <li key={number} className="genres__genre">{genre}</li>
           ))}
