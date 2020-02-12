b89977f49a80fb75947320fe577f670fa607bba3
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-12 16:07:24 +0900

    buildRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App(){
-  return <span>Something</span>
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={home} />
+      <Route path="/about" component={about}/>
+    </HashRouter>
+  );
 }
 
 export default App;
